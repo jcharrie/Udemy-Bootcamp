6935ca3074e934e839791a1821690926ced622dd
Jean-Claude Charrier <charrierjeanclaude@yahoo.com>
2017-07-11 19:43:26 -0700

    12.138
    this keyword use

--- Section 12/methods.js
+++ Section 12/methods.js
@@ -8,3 +8,12 @@ catspace.speak = function(){
 dogspace.speak= function(){
   return "WOOF";
 }
+
+var comments = {};
+comments.data = ["Good Job!", "Bye", "Lame..."];
+
+comments.print = function(){
+  this.data.forEach(function(el){
+    console.log(el);
+  });
+}
