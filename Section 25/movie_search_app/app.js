ccdb4c738fdb0e7c78a8ab0f6a8782f64f86577a
Jean-Claude Charrier <charrierjeanclaude@yahoo.com>
2017-07-28 17:04:17 -0700

    25.244
    Movie API App: adding search

--- Section 25/movie_search_app/app.js
+++ Section 25/movie_search_app/app.js
@@ -6,8 +6,14 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+  res.render("search");
+});
+
 app.get("/results", function(req, res){
-  request('http://www.omdbapi.com/?s=iowa&apikey=thewdb', function(error, response, body){
+  var query = req.query.search;
+  var url = "http://omdbapi.com/?s=" + query + "&apikey=thewdb";
+  request(url, function(error, response, body){
     if(!error && response.statusCode == 200){
       var data = JSON.parse(body);
       res.render("results", {data: data});
