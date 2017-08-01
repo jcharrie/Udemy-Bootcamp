550bab378286bd614dd6a628fef0040310587e22
Jean-Claude Charrier <charrierjeanclaude@yahoo.com>
2017-07-31 19:50:15 -0700

    26.250
    YelpCamp: Styling nav and forms

--- Section 26/YelpCamp/v1/app.js
+++ Section 26/YelpCamp/v1/app.js
@@ -9,6 +9,9 @@ var campgrounds = [
   {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
   {name: "Granite Hill", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg"},
   {name: "Mountain Goat's Nest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
+  {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+  {name: "Granite Hill", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg"},
+  {name: "Mountain Goat's Nest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
 ];
 
 app.get("/", function(req, res){
