05e22d8a19310f5fa8b3c505347ff748e052b5d0
Jean-Claude Charrier <jcharrie@ucsc.edu>
2017-08-14 18:50:45 -0700

    33.290
    yelpcamp auth pt.2

--- Section 33/YelpCamp/v6/app.js
+++ Section 33/YelpCamp/v6/app.js
@@ -10,11 +10,23 @@ var express       = require("express"),
     seedDB        = require("./seeds");
 
 mongoose.Promise = global.Promise; 
-mongoose.connect("mongodb://localhost/yelp_camp_v3", {useMongoClient: true});
+mongoose.connect("mongodb://localhost/yelp_camp_v6", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+  secret: "YelpCamp secret",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", function(req, res){
@@ -107,6 +119,28 @@ app.post("/campgrounds/:id/comments", function(req, res){
   // create new comment
   // conect new comment to campground
   // redirect campground show page
+});
+
+// =============
+// AUTH ROUTES
+// =============
+
+// show register form
+app.get("/register", function(req, res){
+  res.render("register");
+});
+// handle sign up logic
+app.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/campgrounds");
+    });
+  });
 });
 
 app.listen(3000, function(){
