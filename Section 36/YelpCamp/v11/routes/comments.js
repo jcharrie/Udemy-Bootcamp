cc7563f383b11763e35d0dab30324f5423127160
Jean-Claude Charrier <jcharrie@ucsc.edu>
2017-09-16 15:26:09 -0700

    36.310
    flash messages: helpful errors

--- Section 36/YelpCamp/v11/routes/comments.js
+++ Section 36/YelpCamp/v11/routes/comments.js
@@ -22,11 +22,13 @@ router.post("/", middleware.isLoggedIn, function(req, res){
   // lookup campground using ID
   Campground.findById(req.params.id, function(err, campground){
     if(err){
+      req.flash("error", "Something went wrong");
       console.log(err);
       res.redirect("/campgrounds");
     } else{
       Comment.create(req.body.comment, function(err, comment){
         if(err){
+          req.flash("error", "Something went wrong");
           console.log(err);
         } else{
           // add username and id to comment
@@ -37,6 +39,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
           campground.comments.push(comment);
           campground.save();
           // console.log(comment);
+          req.flash("success", "Successfully added comment");
           res.redirect("/campgrounds/" + campground._id);
         }
       });
@@ -72,6 +75,7 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, re
     if(err){
       res.redirect("back");
     } else{
+      req.flash("success", "Comment deleted");
       res.redirect("/campgrounds/" + req.params.id);
     }
   });
--- Section 36/YelpCamp/v11/routes/index.js
+++ Section 36/YelpCamp/v11/routes/index.js
@@ -18,10 +18,12 @@ router.post("/register", function(req, res){
   var newUser = new User({username: req.body.username});
   User.register(newUser, req.body.password, function(err, user){
     if(err){
-      console.log(err);
-      return res.render("register");
+      // console.log(err);
+      req.flash("error", err.message);
+      return res.redirect("/register");
     }
     passport.authenticate("local")(req, res, function(){
+      req.flash("success", "Welcome to YelpCamp " + user.username);
       res.redirect("/campgrounds");
     });
   });
@@ -47,13 +49,5 @@ router.get("/logout", function(req, res){
   req.flash("success", "Logged you out!");
   res.redirect("/campgrounds");
 });
-
-// Middleware
-function isLoggedIn(req, res, next){
-  if(req.isAuthenticated()){
-    return next();
-  }
-  res.redirect("/login");
-}
 
 module.exports = router;
